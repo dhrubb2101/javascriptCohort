0ecb562ed6638a2a62054eba2dacde99b5e2c7df
dhrubb <dhrubbgupta@gmail.com>
2025-06-06 07:20:56 +0530

    learnt about funtion,how to create one , how to understand it's syntax. Basically function is a factory.

---
+++ hello.js
@@ -0,0 +1,16 @@
+//factory ka syntac seekh lo saari problem khtam ho jayengi js mei
+//function matlab factory
+function printChai(){
+    console.log("Hello Chai")
+}
+
+function bringBrush(itne){
+    console.log(`Haanji,le aaya brush, ${itne} brush.`)
+}
+
+
+printChai();
+bringBrush()
+//function ka naam printChai hai
+//function ko call karne ke liye printChai() likhna padta hai
+//if we want to call bringBrush function first we will write bringBrush() above printChai()
