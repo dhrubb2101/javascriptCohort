93a1bcd98e826278851a543ebf7f5fd98a9dd037
dhrubb <dhrubbgupta@gmail.com>
2025-06-07 20:25:28 +0530

    5 datatypes are mainly used - objects, arrays , string,number, boolean , null , undefined

---
+++ bootcampProfile.js
@@ -0,0 +1,34 @@
+//when ram asks for some space we use let and const 
+//string - sequence of characters
+//you can declare it using three ways - '',"",``
+
+let name = "Rohit"
+//Number - 25, 25.9
+let age = 25.5;
+//boolean - true or false
+let isPaid = true;
+
+let favoriteClass = null
+//null means nothingness, it is empty
+
+//undefined means baad mei dekhenge, like has defined that it is empty well undefined means 
+//it has nothing, nothing has been defined in it.
+//like for null it is defined it is empty.
+
+let hometown; 
+// console.log(favoriteClass);
+// console.log(hometown);
+
+//It's an array datatype
+let skills = ["html","css","Javascript"]
+
+//It'sobject datatype
+let studentProfile = {
+    name : "Rohit",
+    age : 22,
+    isPaid : true,
+    skills : ["html","CSS","Javascript"],
+    favoriteClass : null,
+}
+
+//These are the most important data types in javascript and most used.
