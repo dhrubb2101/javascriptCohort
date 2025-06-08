81777c8d6e3508cc3753d292d1dba8757c772981
dhrubb <dhrubbgupta@gmail.com>
2025-06-08 09:13:49 +0530

    pizza challenge if conditional

---
+++ pizza.js
@@ -0,0 +1,19 @@
+let numberofGuest = 4;
+
+let pizzaSize;
+//small <= 2
+//medium <= 5
+//large
+
+if(numberofGuest <= 2){
+    pizzaSize = "small";
+    console.log('Pizza size is ${pizzaSize');
+}else if(numberofGuest <= 5){
+    pizzaSize = "medium"
+    console.log(`Pizza size is ${pizzaSize  }`)
+}else{
+    pizzaSize = "large"
+    console.log(`Pizza size is ${pizzaSize}`)
+}
+
+console.log(pizzaSize)
