d722c5cb6382323d2928e6cb675a7d4b9bbae9fc
dhrubb <dhrubbgupta@gmail.com>
2025-06-09 07:54:06 +0530

    learning aboutg arrays (arrays.js)

---
+++ array.js
@@ -0,0 +1,22 @@
+let fruits = ["apple","cherry","banana"];
+let intFruits = new Array("kiwi","avacode","dragonFruit")
+
+// console.log(fruits);
+// console.log(intFruits)
+
+// console.log(typeof fruits);
+// console.log(typeof intFruits);
+
+// //type of array is object
+
+// console.log(fruits[0]); //apple
+// console.log(frruits.length); //3
+
+fruits[0] = "bleberry";
+
+//methods are lagi hui factories jo pehle se bani hui hai - predefined factory
+
+fruits.push("new item") // adds item to end
+fruits.unshift("new item") // adds item to beginning
+fruits.pop() //removes last item
+console.log(fruits)
