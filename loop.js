2d43d46ede21d8522f2e24caecd51864dae14053
dhrubb <dhrubbgupta@gmail.com>
2025-06-12 07:52:43 +0530

    loops- how to construct them in js

---
+++ loop.js
@@ -0,0 +1,17 @@
+let teas = ["masala","ginger","oolong","green","black","white"];
+
+teas[h]
+
+teas.length // 6
+
+let h = 5 //value of h will remain h<=5 | h<6
+
+// loop - iteration; i
+//pehla step variable lekr aao sabse pehlee
+//dusra step for loop is limit btao kaha tk jaana hai
+//tessra step increment or decrement => h = h+1 => h++
+
+for(let h = 0; h < teas.length; h = h+1){
+    
+
+}
