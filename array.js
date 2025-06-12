453c6e2832ef27eb93649a4af3e913322c6b1093
dhrubb <dhrubbgupta@gmail.com>
2025-06-12 07:36:00 +0530

    arrays complete

--- array.js
+++ array.js
@@ -4,19 +4,20 @@ let intFruits = new Array("kiwi","avacode","dragonFruit")
 // console.log(fruits);
 // console.log(intFruits)
 
-// console.log(typeof fruits);
-// console.log(typeof intFruits);
+// console.log(typeof fruits); //object
+// console.log(typeof intFruits); //object
 
 // //type of array is object
 
 // console.log(fruits[0]); //apple
 // console.log(frruits.length); //3
 
-fruits[0] = "bleberry";
+fruits[0] = "bleuberry";
 
 //methods are lagi hui factories jo pehle se bani hui hai - predefined factory
 
 fruits.push("new item") // adds item to end
 fruits.unshift("new item") // adds item to beginning
-fruits.pop() //removes last item
+fruits.pop() //removes last item and returns it
+fruits.shift() //removes first item and returns it
 console.log(fruits)
