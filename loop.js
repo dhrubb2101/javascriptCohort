6dc80316a828b7357c28b8cec13ab997b29897aa
dhrubb <dhrubbgupta@gmail.com>
2025-06-13 14:31:24 +0530

    for loop

--- loop.js
+++ loop.js
@@ -1,6 +1,6 @@
 let teas = ["masala","ginger","oolong","green","black","white"];
 
-teas[h]
+// teas[h]
 
 teas.length // 6
 
@@ -10,8 +10,8 @@ let h = 5 //value of h will remain h<=5 | h<6
 //pehla step variable lekr aao sabse pehlee
 //dusra step for loop is limit btao kaha tk jaana hai
 //tessra step increment or decrement => h = h+1 => h++
-
-for(let h = 0; h < teas.length; h = h+1){
-    
+//koi aisa point aana chahiye jaha condition satisdy na ho
+for(let h = 0; h < teas.length; h++){
+    console.log(`Tea at index ${h}: ${teas[h]}`)
 
 }
