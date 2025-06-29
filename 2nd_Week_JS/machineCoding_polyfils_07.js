6eb4361bfde05312ded1c2fc1db209e4d4d113b3
dhrubb <dhrubbgupta@gmail.com>
2025-06-29 16:36:28 +0530

    Another example of polyfils in machinCoding_poulfile_07.js

--- 2nd_Week_JS/machineCoding_polyfils_07.js
+++ 2nd_Week_JS/machineCoding_polyfils_07.js
@@ -1,4 +1,4 @@
-const arr = [1, 2, 3,4,5,6];
+
 
 //interviewer will ask you to write a polyfil for this thing
 
@@ -15,21 +15,67 @@ const arr = [1, 2, 3,4,5,6];
 //it calls my function for every value
 
 
-const ret = arr.forEach(function(value,index){
-    console.log(`Value at Index is ${index} is ${value}`)
+
+
+// so how to create a polyfil for it
+if(!Array.prototype.myForEach){
+    Array.prototype.myForEach = function(userFn){
+        const  originalArr = this; //Current Object ki taraf point krta hai
+
+        for(let i = 0; i< originalArr.length;i++){
+            userFn(originalArr[i],i)
+
+
+        }
+    }
+
+}
+
+const arr = [1, 2, 3,4,5,6];
+
+
+const ret = arr.myForEach(function(value,index){
+    console.log(`My forEach Value at Index is ${index} is ${value}`)
 
 })
 
 console.log(`Ret is`,ret);
 
-//so how to create a polyfil for it
-if(!Array.prototype.myForEach){
-    Array.prototype.myForEach = function(userFn){
-        const  originalArr = this //Current Object ki taraf point krta hai
+//So what we did is we created a polyfil for the forEach function
+//so that it will work on all browsers
+//Basically we created a new function called myForEach
+//which will take a user function as an input parameter
+//and it will call that function for each element in the array      
+//and it will pass the value and index as parameters to the user function
+//and this will point to the current object
+//which is the array on which the myForEach function is called
 
-        for(let i = 0; i< originalArr.length;i++){
+//Now taking another example of polyfil
+
+//we are taking a map 
+//Signature .map
+// Returns a new array , Each element ke upar iterate krta hai aur 
+//new array is basically jo bhi user ka function new return krta hai usse new array bana kr de deta hai
+
+const n = arr.map((e) => e * 2) // n here would be a completely new array where all elements would be double
+const n2 = arr.myMap((e) => e * 3) // n2 here would be a completely new array where all elements would be triple
+console.log(n);
+console.log(arr)
+console.log(n2);
+
+
+if(!Array.prototype.myMap){
+    Array.prototype.myMap = function(userFn){
+        const result = []
+
+        for(let i = 0; i < this.length; i++){
+            const value = userFn(this[i],i)
+            result.push(value)
 
         }
-    }
 
+
+        return result
+
+    }
 }
