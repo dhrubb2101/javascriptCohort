a1a3812e705b386dccaf9b4b01561c5088948dc5
dhrubb <dhrubbgupta@gmail.com>
2025-07-03 07:01:26 +0530

    learnt about objects!

--- 3rd_Week_JS/objects_01.js
+++ 3rd_Week_JS/objects_01.js
@@ -3,3 +3,16 @@
 //and those entities have properties and methods
 //for example a car is a real world entity
 //and it has properties like color, model, brand, etc.
+
+//objects have properties and methods
+//properties are the attributes of the objects 
+//like color, model, brand, etc.
+//Methods are the actions,functions that the object can perform
+//like start, stop, accelerate, etc.
+
+//What is the difference between an function and method
+//There's no actual difference between a function and a method
+//A method is a function that is associated with an object
+//So when we say method, we mean a function that is associated with an object
+//So when we say function, we mean a standalone function
+
