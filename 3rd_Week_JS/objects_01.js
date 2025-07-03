e73b0457f542f12083f724a892e94fda0a95c1da
dhrubb <dhrubbgupta@gmail.com>
2025-07-03 06:45:33 +0530

    learning of objects!

---
+++ 3rd_Week_JS/objects_01.js
@@ -0,0 +1,5 @@
+//objects represents the real world entities
+//everything are a real world entity 
+//and those entities have properties and methods
+//for example a car is a real world entity
+//and it has properties like color, model, brand, etc.
