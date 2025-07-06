e025937d4ccb0a32ccdefcd59bea6b3bbf54e5ca
dhrubb <dhrubbgupta@gmail.com>
2025-07-06 16:34:22 +0530

    DOM manipulation and index.html

---
+++ 3rd_Week_JS/DOM_04.js
@@ -0,0 +1,6 @@
+//using js you can manipulate the DOM (basically the HTML structure)
+// and you can add, remove, modify the elements in the DOM
+// and you can also add event listeners to the elements in the DOM  
+
+//we can run js code in the browser and in the node (outside the browser)
+
