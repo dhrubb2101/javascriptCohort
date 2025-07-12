dec4b2875973142866b32939fbbdcfed52cd9092
dhrubb <dhrubbgupta@gmail.com>
2025-07-12 08:57:18 +0530

    learning about defining a custom type using interface ( we do that using typechecking)

--- 8th_Week_TypeScript/hello_01.ts
+++ 8th_Week_TypeScript/hello_01.ts
@@ -58,11 +58,58 @@ function add(x:number,y:number){
     return `x+y`
 }
 //add('piyush',3)// Error: Argument of type 'string' is not assignable to parameter of type 'number'.
-
-function createUser(){
-
+function createUser(user : { firstName: string, lastName?: string}){
+    if(!user.lastName) return
+    const trimmedLastName: string = user.lastName?.trim() || '';
 }
+
+createUser({
+    firstName:"piyush",
+    lastName:"dhrurb",
+})
 
 let x = add(4,3)
 //x*300// Error: Type 'string' is not assignable to type 'number'.
 //x.toFixed(2) //not possible since x is a string
+
+//=====================================
+
+//In Javascript errors come in runtime
+//In TypeScript errors come in BuildTime Error and we can fix them before running the code
+//And another during runtime also.
+//So two types of errors in typescript
+//1. BuildTime Error
+//2. Runtime Error
+
+//======================================
+
+//In typescript, there's whole art for"HOW YOU DEFINE TYPES"
+
+interface User { //we build here a custom type called 'User'
+    firstName : string
+    lastName?: string
+    email:string
+    profileImageURL?: string
+}
+
+//interface is basically a blueprint for an object
+//it defines the structure of an object 
+//and it can be used to create objects of that type
+
+function updateUser(user : User){
+    user
+}
+
+const payload : User = {
+    firstName: '',
+    email: ''
+}
+updateUser(payload)
+
+//In the above code, we have defined a type 'User' using an interface
+//and we have used that type to create an object 'payload'
+//and we have used that object to call the function 'updateUser'
+
+//======================================
+
+// function doSomething(x:number)
