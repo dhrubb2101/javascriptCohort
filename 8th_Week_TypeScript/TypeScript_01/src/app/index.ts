a867edaa7f866e3411a3d8963b335c3f12d1f3cd
dhrubb <dhrubbgupta@gmail.com>
2025-07-13 11:44:04 +0530

    Typescript usage in boiler plate code for production setup

--- 8th_Week_TypeScript/TypeScript_01/src/app/index.ts
+++ 8th_Week_TypeScript/TypeScript_01/src/app/index.ts
@@ -2,7 +2,7 @@
 //jb yeh compile hogi toh yeh const require mei convert ho jayegi
 // yeh import mei use krne ka fayda yeh hai ki mujhe type checking milti hai
 //typescript is a typed language
-import fs from 'fs';
+//import fs from 'fs';
 //the problem here is i am typescript and i don't know the types of fs
 //@types/node is a package that contains the types for node.js
 //jitne bi node ke internal modules hain unke types yeh package provide karta hai
@@ -10,3 +10,18 @@ import fs from 'fs';
 //why dev dependency? because we don't need it in production
 //we only need it for development
 
+import http from 'http'
+
+async function main(){
+    try{
+
+        const PORT: number = +(process.env.PORT ?? 8000)
+        
+        const server = http.createServer()
+        server.listen()
+
+
+    }catch (err){
+
+    }
+}
