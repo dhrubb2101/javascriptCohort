d3deddebe1a43c733e6a2f2df5f4d8ab0a004063
dhrubb <dhrubbgupta@gmail.com>
2025-07-15 03:07:48 +0530

    create react and react-dom objects from raw file of unpkg of their respective ones

---
+++ 1st_Week_React/react_01/src/app.js
@@ -0,0 +1,13 @@
+const App = () => {
+    return React.createElement( //this is 
+        "div",
+        {},
+        React.createElement(
+            "h1",
+            {},
+            "Chai, chill and react - React - 18"
+        )
+    )
+}
+
+const container = document.getElementById('root')
