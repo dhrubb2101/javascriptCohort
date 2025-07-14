58adeb73490b995e679e9b0dc3ce04182bfc510a
dhrubb <dhrubbgupta@gmail.com>
2025-07-15 03:55:51 +0530

    chapter 2 done , in which we understand how react works

--- 1st_Week_React/react_01/src/app.js
+++ 1st_Week_React/react_01/src/app.js
@@ -1,3 +1,6 @@
+import React from "https://esm.sh/react@19.1.0"
+import ReactDOM from "https://esm.sh/react-dom@19.1.0/client"
+
 const App = () => {
     return React.createElement( //this is 
         "div",
@@ -5,9 +8,15 @@ const App = () => {
         React.createElement(
             "h1",
             {},
-            "Chai, chill and react - React - 18"
+            "Chai, chill and react - React - 18" //this is children - which can be an array where you can create multiple elements or it can be single
         )
     )
 }
 
 const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+root.render(React.createElement(App))
+
+//ReactDOM is react ka extension in browser
+//React.createElement is used to create elements in React
+//ReactDOM.render is used to render the React element into the DOM
