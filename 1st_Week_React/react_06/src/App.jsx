04ee59adc3d5f55af75e515d83e64692f10f14a8
dhrubb <dhrubbgupta@gmail.com>
2025-07-18 06:51:57 +0530

    working with web server (API calls)

---
+++ 1st_Week_React/react_06/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export function App(){
+    return (
+        <div>
+            <h1>welcome to chaicode </h1>
+            <p>Serving hot chai with react</p>
+        </div>    
+    )
+}
---
+++ 1st_Week_React/react_06/vit.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';   
+
+// https://vitejs.dev/config/
+export default defineConfig({   
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://localhost:3000', // Adjust the target URL as needed
+                changeOrigin: true,
+            },
+        },
+    },
+});
