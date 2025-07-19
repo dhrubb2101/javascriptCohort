9e4533b9043a92c524c833205e5266acb1042986
dhrubb <dhrubbgupta@gmail.com>
2025-07-19 07:06:37 +0530

    how to use useState in react and how it is written in [variable,setVariable] = useState(initial value)

--- 1st_Week_React/react_06/src/App.jsx
+++ 1st_Week_React/react_06/src/App.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import { useState } from "react"
 
 export function App(){
-    const [message, setMessage] = useState("Loading") //const[var,fn] = useState(initialValue)
+    const [message, setMessage] = useState("Loading...") //const[var,fn] = useState(initialValue)
     return (
         <div>
             <h1>welcome to chaicode </h1>
             <p>Serving hot chai with react</p>
+            <h2>{message}</h2>
         </div>    
     )
 }
