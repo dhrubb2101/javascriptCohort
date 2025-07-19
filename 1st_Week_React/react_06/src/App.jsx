ce54a2f265d61771f11113c0af68d08d247b32c6
dhrubb <dhrubbgupta@gmail.com>
2025-07-19 08:02:35 +0530

    understood useState and useEffect in react in a very new way

--- 1st_Week_React/react_06/src/App.jsx
+++ 1st_Week_React/react_06/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react"
 import { useState } from "react"
+import { set } from "zod"
 
 export function App(){
     const [message, setMessage] = useState("Loading...") //const[var,fn] = useState(initialValue)
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/api`)
+        .then((response) => response.json())
+        .then((data) => setMessage(data.message))
+        .catch((error) => setMessage("Error fetching data"))
+        //there's no server set up here, so this will not work
+    }, [])
     return (
         <div>
             <h1>welcome to chaicode </h1>
