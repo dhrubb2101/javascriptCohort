85430718ca516f6fe3a85f4df78835130b257627
dhrubb <dhrubbgupta@gmail.com>
2025-08-10 14:27:52 +0530

    numericals practice (challenges on chaicode) dry-run and what you understand by question

---
+++ Numericals/loops.js
@@ -0,0 +1,6 @@
+// there are two paramters - totalGifts and friends
+// so there are three scenarios 
+// one where totalGifts and friends are equal or total gifts are more and another one is
+// where totalGifts is less than friends and return total gifts
+
+
