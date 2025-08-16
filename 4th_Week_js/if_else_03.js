b4760d7ff4265e8cda5f6e0b09fbc60a604d2ee8
dhrubb <dhrubbgupta@gmail.com>
2025-08-16 07:56:28 +0530

    function is a factory

---
+++ 4th_Week_js/if_else_03.js
@@ -0,0 +1,15 @@
+function prepareChai(type){
+    if(type === "Masala Chai"){
+        console.log("Adding spices to the chai")
+    }else{
+        console.log("Preparing regular chai")
+    }
+}
+//basic syntax for funtion declaration
+//it is a factory where some work tak place
+
+prepareChai("Masala Chai")  // Output: "Adding spices to the chai"
+prepareChai("Green Chai")  // Output: "Preparing regular chai"
+
+//========================================================
+
