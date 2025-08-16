dbeedd83239e31e7b8aec6cdd952fed546a9bc2e
dhrubb <dhrubbgupta@gmail.com>
2025-08-16 07:17:32 +0530

    to merge two arrays

--- 4th_Week_js/array_and_objects_02.js
+++ 4th_Week_js/array_and_objects_02.js
@@ -2,5 +2,55 @@ let chaiTypes = ["Masala Chai", "Ginger Chai","Green Chai" ,"Lemon Chai"];
 
 console.log(chaiTypes[2]);
 
+//========================================================
+
 console.log(`Total chai Types: ${chaiTypes.length}`)
 
+chaiTypes.push("Herbal Tea") // adds item to end
+
+const data = chaiTypes.pop() //removes last item and returns it
+
+console.log(data)   // Output: "Herbal Tea"
+
+let index = chaiTypes.indexOf("Green-Chai"); // output: 2
+console.log(index)
+
+//========================================================
+
+if(index !== -1) {
+    chaiTypes.splice(index, 1); // removes "Green Chai" from the array
+}
+console.log(chaiTypes); // Output: ["Masala Chai", "Ginger Chai", "Lemon Chai"]
+
+//========================================================
+
+//Array and using loops on it(very common in JS)
+
+chaiTypes.forEach((chai, index) => {
+    console.log(`${index + 1}: ${chai}`)
+})
+
+//================================
+
+//how to merge two arrays
+let moreChaiTypes = ["Oolong Tea", "White Tea"]
+
+let allChaiTypes = chaiTypes.concat(moreChaiTypes)
+
+let newChaiTypes = [...chaiTypes, "Chamomile Tea"] //spread operator basically spreads the elements of the array chaiTypes and adds "Chamomile Tea" to the end
+console.log(allChaiTypes); // Output: ["Masala Chai", "Ginger Chai", "Lemon Chai", "Oolong Tea", "White Tea"]
+console.log(newChaiTypes); // Output: ["Masala Chai", "Ginger
+
+//=========================================================
+
+//object literals
+
+let chaiRecipe = {
+    name: "Masala Chai",
+    ingredients: {
+        teaLeaves: "Assam Tea",
+        milk: "Full Cream Milk",
+        spices: ["DaalChini","Ginger"]
+    },
+    instruction:"Boil water,add tea leaves,milk, sugar and spices"
+}
