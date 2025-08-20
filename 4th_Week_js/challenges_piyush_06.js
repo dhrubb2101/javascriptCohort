69bb93186333dc3e1104b49ae6b6da2ddd221f80
dhrubb <dhrubbgupta@gmail.com>
2025-08-20 22:00:43 +0530

    chaicode loops challenges last one

--- 4th_Week_js/challenges_piyush_06.js
+++ 4th_Week_js/challenges_piyush_06.js
@@ -179,3 +179,38 @@ Promise.resolve().then(() => {
 
 
 console.log("Bye")
+
+//===================================================================
+
+    function shinyDiamondRug(n) {
+    let spaces = 0;
+    let stars = 0;
+    for(let i = 0; i < n; i++){
+        let row = "";
+
+        for(let j = 0; j < n-i; j++){
+            row = row + " "
+        }
+
+        for(let j = 0; j < 2*i - 1; j++){
+            row = row + "*"
+        }
+        console.log(row)
+    }
+    for(let i = n; i > 0; i--){
+        let row = ""
+        
+        for(let j = 0; j < n - i ; j++){
+            row = row + " "
+        }
+        for(let j = 0; j < 2*i - 1; j++){
+            row = row + "*"
+        }
+        console.log(row)
+    }
+}
+
+shinyDiamondRug(4)
+
+//==================================================
+
