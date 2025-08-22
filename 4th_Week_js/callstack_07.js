d75091550146cb680b5d5844c041b758a43b072a
dhrubb <dhrubbgupta@gmail.com>
2025-08-23 05:24:05 +0530

    global execution context

--- 4th_Week_js/callstack_07.js
+++ 4th_Week_js/callstack_07.js
@@ -28,19 +28,46 @@
 //output is age is undefined
 //=========================================
 
-test(); 
+// test(); 
 
-function test(){
-    console.log('I am inside the test function')
-}
+// function test(){
+//     console.log('I am inside the test function')
+// }
 
-age = 3000 
+// age = 3000 
 
-console.log('Age is', age);
+// console.log('Age is', age);
 
-var age = 24;
+// var age = 24;
 
 //output is age is 3000
 
 //=========================================
+
+// test(); 
+
+// function test(){
+//     console.log('I am inside the test function')
+// }
+
+// age = 3000 
+
+// console.log('Age is', age);
+
+// let age = 24;
+
+//output is age is error - cannot access age before initialization
+
+//=========================================================
+
+//Global Execution Context
+//whenever you run a code before that it's global execution context gets created
+//It has two phases 
+//1st Part is Memory Phase
+//2nd Part is Code Phase
+//whatever variables or function we may have gets stored in memory phase.
+var age = 25;
+
+console.log('Age is ', age);
+
 
