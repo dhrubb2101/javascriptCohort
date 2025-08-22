4304fc4999cf180aa76ecee2eae5ad97cedd4372
dhrubb <dhrubbgupta@gmail.com>
2025-08-22 07:20:20 +0530

    placing og function call with let,var,const to see what works and not

--- 4th_Week_js/callstack_07.js
+++ 4th_Week_js/callstack_07.js
@@ -1,3 +1,46 @@
-const age = 25;
+// const age = 25;
 
-console.log('Age is', age)
+// console.log('Age is', age);
+
+//here age is 25
+
+//=========================================
+
+// console.log('Age is', age);
+
+// const age = 25;
+
+//will throw an error saying "Uncaught ReferenceError: age is not defined"
+
+//=========================================
+
+// test(); 
+
+// function test(){
+//     console.log('I am inside the test function')
+// }
+
+
+// console.log('Age is', age);
+
+// var age = 24;
+
+//output is age is undefined
+//=========================================
+
+test(); 
+
+function test(){
+    console.log('I am inside the test function')
+}
+
+age = 3000 
+
+console.log('Age is', age);
+
+var age = 24;
+
+//output is age is 3000
+
+//=========================================
+
