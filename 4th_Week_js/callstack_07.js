968bf0cf110c4c843ff1cfeaf749f8c9437f49ee
dhrubb <dhrubbgupta@gmail.com>
2025-08-24 04:09:49 +0530

    debugging in js via javascript debug terminal

--- 4th_Week_js/callstack_07.js
+++ 4th_Week_js/callstack_07.js
@@ -66,8 +66,18 @@
 //1st Part is Memory Phase
 //2nd Part is Code Phase
 //whatever variables or function we may have gets stored in memory phase.
+//in memory phase every variable is initialized with undefined
+console.log('Age is ', age);
+
 var age = 25;
 
 console.log('Age is ', age);
 
 
+
+//in code phase , code gets executed line by line
+//so when first executed console.log('Age is ', age); age here is a variable, it will search for it in memory phase.
+//since age variable is present, no error will come here it will just age is undefined,
+//as in memory phase it is initialized with undefined
+//in code phase, next we assign value of 25 to age (when we run code var age = 25)
+//
