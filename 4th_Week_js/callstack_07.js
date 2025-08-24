7bfa4630c28ce2cc44bb2604e451b51a1cf200b5
dhrubb <dhrubbgupta@gmail.com>
2025-08-24 07:50:04 +0530

    hoisting in var

--- 4th_Week_js/callstack_07.js
+++ 4th_Week_js/callstack_07.js
@@ -67,11 +67,12 @@
 //2nd Part is Code Phase
 //whatever variables or function we may have gets stored in memory phase.
 //in memory phase every variable is initialized with undefined
-console.log('Age is ', age);
 
-var age = 25;
+// console.log('Age is ', age);
 
-console.log('Age is ', age);
+// var age = 25;
+
+// console.log('Age is ', age);
 
 
 
@@ -81,3 +82,64 @@ console.log('Age is ', age);
 //as in memory phase it is initialized with undefined
 //in code phase, next we assign value of 25 to age (when we run code var age = 25)
 //
+
+//this loading of variable before hand is called hoisting in js
+//hoisting allows us to use variables before they are declared
+
+//================================================================================
+
+//Do functions get hoisted? Answer is YES
+//In memory Phase - both age variable and test function will be hoisted
+//Age will remain undefined but test function gets hoisted along with it's body and you can see it while debugging
+//This process is known as hoisting. 
+
+
+// test();
+
+// console.log('Age is ', age);
+
+// var age = 25;
+
+// function test(){
+//     console.log('Testing')
+// }
+
+// console.log('Age is ', age);
+
+
+//===============================================================================
+
+
+// console.log('Age is ', age);
+
+// var age = 25;
+
+// var test = function test(){
+//     console.log('Testing')
+// }
+
+// console.log('Age is ', age);
+
+// test()
+
+//Both age variable and test variable will get hoisted 
+//their values would be undefined 
+//even test function is being declared as a variable that's why it's value would also be undefined
+//when in debugger when you reach line 118 
+// it equals value of test variable to a function so if you call test afterwards, it will get called as a function
+
+//==================================================================
+
+//let doesn't get hoisted
+//but in reality let and const are hoisted to the top of their block scope
+//but they are not initialized
+//so accessing them before declaration will result in a ReferenceError
+
+//in both let and const 
+//there is a concept known as "Temporal Dead Zone" (TDZ)
+//so anything above first you declare a variable
+
+console.log('Age is ', age);
+
+let age = 25;
+
