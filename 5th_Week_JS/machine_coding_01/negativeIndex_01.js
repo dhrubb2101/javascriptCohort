5a3d854f3438e7e832e81e45d5b9673bee4c346e
dhrubb <dhrubbgupta@gmail.com>
2025-08-28 03:18:29 +0530

    creating a new proxy object

--- 5th_Week_JS/machine_coding_01/negativeIndex_01.js
+++ 5th_Week_JS/machine_coding_01/negativeIndex_01.js
@@ -1,1 +1,23 @@
-//
+const { negative } = require("zod");
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+
+console.log(arr[-1]);
+
+const user = {
+    name: "hitesh",
+    age: 40,
+    password: "123"
+}
+
+const proxyUser = new Proxy(user, {
+
+    get(target, prop){
+        return target[prop] //target is user object
+    }
+})
+console.log(proxyUser.password)
+
+//some properties we need to hide for that we use proxyObject
+// proxyUser.password
