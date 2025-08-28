8c17817d0a1b2c381f95563d76bb734649f9aacf
dhrubb <dhrubbgupta@gmail.com>
2025-08-28 08:19:17 +0530

    proxy object and get method usage

--- 5th_Week_JS/machine_coding_01/negativeIndex_01.js
+++ 5th_Week_JS/machine_coding_01/negativeIndex_01.js
@@ -1,9 +1,10 @@
 const { negative } = require("zod");
 
-let arr = [1,2,3,4,5,6,7,8,9,10]
+// let arr = [1,2,3,4,5,6,7,8,9,10]
 
+// console.log(arr[-1]);
 
-console.log(arr[-1]);
+//===========================
 
 const user = {
     name: "hitesh",
@@ -12,12 +13,29 @@ const user = {
 }
 
 const proxyUser = new Proxy(user, {
-
     get(target, prop){
+
+        console.log(prop)
+        // if(prop === "password"){
+        //     throw new Error("Access Denied")   
+        // }
+
+
         return target[prop] //target is user object
     }
 })
-console.log(proxyUser.password)
+console.log(proxyUser.age)
+
+// In this code, prop represents the property name being accessed on the proxyUser object.
+
+// When you write:
+// console.log(proxyUser.age);
+// The get trap is triggered, and the prop parameter receives "age" as its value.
+
+// So:
+// If you do proxyUser.name → prop is "name".
+// If you do proxyUser.password → prop is "password".
+// In short, prop is a string (or symbol) that tells you which property was requested from the proxy.
 
 //some properties we need to hide for that we use proxyObject
 // proxyUser.password
