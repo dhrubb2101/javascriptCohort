4e40e4d9a559db28e0ea1bb7aab9b3f87f0a0461
dhrubb <dhrubbgupta@gmail.com>
2025-08-29 05:10:12 +0530

    explanation of set and it's usage

--- 5th_Week_JS/machine_coding_01/negativeIndex_01.js
+++ 5th_Week_JS/machine_coding_01/negativeIndex_01.js
@@ -12,19 +12,45 @@ const user = {
     password: "123"
 }
 
-const proxyUser = new Proxy(user, {
-    get(target, prop){
+// const proxyUser = new Proxy(user, {
+//     get(target, prop){
 
-        console.log(prop)
-        // if(prop === "password"){
-        //     throw new Error("Access Denied")   
-        // }
+//         console.log(prop)
+//         // if(prop === "password"){
+//         //     throw new Error("Access Denied")   
+//         // }
 
 
-        return target[prop] //target is user object
-    }
-})
-console.log(proxyUser.age)
+//         return target[prop] //target is user object
+//     }
+    // set((target, prop, value) => {
+    //     if(prop === "age"){
+    //         if(typeof value !== "number"){
+    //             throw new Error("Age must be a number")
+    //         }
+    //         if(value < 0){
+    //             throw new Error("Age must be a positive number")
+    //         }
+    //     }
+    //     target[prop] = value 
+    // })
+// })
+// console.log(proxyUser.age)
+
+// Explanation
+
+// set(target, prop, value) → This trap intercepts whenever you try to assign a value to a property of the proxy object.
+// target → the original object being proxied (like user)
+// prop → the property being set (like "age")
+// value → the new value being assigned
+
+// Inside the trap:
+// It checks if the property being set is "age".
+// If yes, it validates:
+// Must be a number (typeof value !== "number" → error).
+// Must be positive (value < 0 → error).
+// If the value passes, it assigns it to the original object:
+// target[prop] = value
 
 // In this code, prop represents the property name being accessed on the proxyUser object.
 
