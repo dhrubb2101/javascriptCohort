46fc842d5e8d372d174f71967f6556ccca42b853
dhrubb <dhrubbgupta@gmail.com>
2025-09-02 04:53:30 +0530

    date time project , working on script.js

--- 5th_Week_JS/projects/proj_01/finished/script.js
+++ 5th_Week_JS/projects/proj_01/finished/script.js
@@ -1,1 +1,6 @@
-console.log("test")
+console.log("test") //$0 on browser during inspeact element reresents the current element you're selecting on webpage
+
+const timeElement = document.getElementById("time")
+const dateElement = document.getElementById("date")
+
+const date = new Date()
