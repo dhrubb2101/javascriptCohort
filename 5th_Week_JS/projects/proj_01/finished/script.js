2631345850f83c1f12a046c24687be081d5b4e78
dhrubb <dhrubbgupta@gmail.com>
2025-09-02 07:45:38 +0530

    compiled all of it in a function and then called using a setInterval function

--- 5th_Week_JS/projects/proj_01/finished/script.js
+++ 5th_Week_JS/projects/proj_01/finished/script.js
@@ -1,6 +1,7 @@
 console.log("test") //$0 on browser during inspeact element reresents the current element you're selecting on webpage
 
-const timeElement = document.getElementById("time")
+function updateClock() {
+    const timeElement = document.getElementById("time")
 const dateElement = document.getElementById("date")
 
 const now = new Date()
@@ -10,10 +11,23 @@ const minutes = now.getMinutes().toString().padStart(2, "0")
 console.log(minutes); //added .string.padStart in minutes so that 1 - 9 minutes we have return two digits so always return 01-09 minutes like this so used above operations
 //or another solution could be
 //minutes < 10 ? `0${minutes}` : `${minutes}`;
-const time = 0 || 12
-console.log(time)
+const seconds = now.getSeconds().toString().padStart(2, "0")
+const ampm = now.getHours() >= 12 ? "PM" : "AM"
+// const time = 0 || 12
+// console.log(time)
 // const minutes = now.getMinutes()
 // const seconds = now.getSeconds()
 // const ampm = hours >= 12 ? "PM" : "AM"
 // const formattedTime = `${String(hours % 12).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")} ${ampm}`
 // timeElement.textContent = formattedTime
+
+//we collected a lot of data here now we have store it.
+
+timeElement.textContent = `${hours}:${minutes}:${seconds}:${ampm}`
+
+}
+//======
+
+
+
+setInterval(updateClock, interval)
