5af8d2c110da5e0d99b810f4b11bc6efddd8ea14
dhrubb <dhrubbgupta@gmail.com>
2025-09-02 06:28:38 +0530

    using gethours(), using truth and falsey in this camp(You ask)

--- 5th_Week_JS/projects/proj_01/finished/script.js
+++ 5th_Week_JS/projects/proj_01/finished/script.js
@@ -3,4 +3,14 @@ console.log("test") //$0 on browser during inspeact element reresents the curren
 const timeElement = document.getElementById("time")
 const dateElement = document.getElementById("date")
 
-const date = new Date()
+const now = new Date()
+const hours = now.getHours() % 12
+console.log(hours)
+
+const time = 0 || 12
+console.log(time)
+// const minutes = now.getMinutes()
+// const seconds = now.getSeconds()
+// const ampm = hours >= 12 ? "PM" : "AM"
+// const formattedTime = `${String(hours % 12).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")} ${ampm}`
+// timeElement.textContent = formattedTime
