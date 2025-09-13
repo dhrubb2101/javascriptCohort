f718b3f1a80afdcf47384742d5ec96cebf913743
dhrubb <dhrubbgupta@gmail.com>
2025-09-13 10:55:48 +0530

    completed forEach polyfill

--- 5th_Week_JS/polyfils_06.js
+++ 5th_Week_JS/polyfils_06.js
@@ -35,3 +35,82 @@
 //we can see all the functionalities which are present in array.prototype
 //arr.__proto__ also gives the same result
 //===========================================================================================================================================
+
+//and now say you run below code
+// arr = [1,2,3]
+//arr.__proto__
+//Array.prototype
+// array.map(e => e*2) //[2,4,6] //it works because map function is present in array.prototype
+// //array.__proto__ = null //if we do this then map function will not work
+// array.map(e => e*2)  //error because map function is not present in array.prototype
+
+
+//===========================================================================
+
+if(!Array.prototype.hululu) {
+    Array.prototype.hululu = function() {
+        // Custom implementation of hululu
+    }
+}
+
+//this is basic a polyfill of hululu function
+// we first check if hululu function is already present in array.prototype or not
+//if it is not present then we add our own implementation of hululu function to array.prototype
+//now we can use hululu function on any array
+//arr.hululu()
+
+//===========================================================================
+
+//shall we make a hululu property in array.prototype
+//yes we can make a custom property in array.prototype
+
+Array.prototype.hululu = function(){
+    console.log("Hululu function called")
+}
+
+const nums = [1,2,3,4]
+
+nums.hululu() //Hululu function called
+
+//because it is present in nums.__proto__ which is same as Array.prototype
+//because Array.prototype is the parent class of all the arrays
+
+//===========================================================================
+
+//now let's make a polyfill of forEach function
+
+//forEach function takes a callback function as an argument
+
+//for ex const nums = [1,2,3,4]
+//nums.forEach( (currentItem, index) => console.log(currentItem, index) )
+//1 0
+//2 1
+//3 2
+//4 3
+
+//here forEach works like a loop and iterates over each item of the array and calls the callback function with currentItem and index as arguments
+
+//array runs user function or callback function for each item of the array
+//===================
+
+if(!Array.prototype.myForEach){
+    Array.prototype.myForEach = function(cb){
+        for(let i=0; i < this.length; i++){
+            cb(this[i],i) //cb is the callback function passed by user
+    }
+    }
+}
+const arr =  [1,2,3,4]
+
+arr.myForEach()
+
+// Why this?
+// Inside your custom myForEach, this refers to the array on which the method was called.
+//So when you loop with for(let i=0; i < this.length; i++), you’re iterating over the elements of the array that called myForEach
+//Why function(cb)?
+//cb (callback) is the function you pass when calling myForEach.
+//That function is called once per element of the array, just like the native .forEach.
+
+//===========================================================================   
+
+
