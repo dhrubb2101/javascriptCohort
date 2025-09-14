c931a14fb0f9f0a03ea521c7f5ccca9058c1a649
dhrubb <dhrubbgupta@gmail.com>
2025-09-14 11:32:03 +0530

    machine coding by hitesh sir ( understanding arguments in a functions (displayed arguments when passed when calling a function

---
+++ 5th_Week_JS/machineCoding_hiteshSir_07.js
@@ -0,0 +1,13 @@
+function ptaNahi(fn, delay){ //these are paramters
+    console.log(arguments) //this will give us the arguments object as an array like object
+    let myId;
+    return function(){
+
+    }
+}
+
+ptaNahi("Hitesh", 2)
+
+//this ptaNahi function is a higher order function because it returns a function
+
+//=====================================================================================
