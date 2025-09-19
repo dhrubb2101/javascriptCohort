fdb9f92f69623365c3c40e230aa39b5f5aa68ec6
dhrubb <dhrubbgupta@gmail.com>
2025-09-19 07:03:06 +0530

    lexicalScoping debugging and showing of closure in debug mode

--- 5th_Week_JS/lexicalScoping_and_closures_09.js
+++ 5th_Week_JS/lexicalScoping_and_closures_09.js
@@ -1,11 +1,23 @@
-let fname = "Piyush"
+// let fname = "Piyush"
 
-function sayName(){
-    console.log(`Inside SayName Fn the value of fname is`, fname)
-}
-console.log("Value of Fname is", fname)
+// function sayName(){
+//     console.log(`Inside SayName Fn the value of fname is`, fname)
+// }
+// console.log("Value of Fname is", fname)
 
-sayName()
+// sayName()
+
+//output
+//Value of Fname is Piyush
+//Inside SayName Fn the value of fname is Piyush
+
+//even though the variable is defined outside the function
+//but still the function is able to access it
+//why and how?
+//the reason is variable outside the function is defined in global scope
+//and the function is able to access the variable from global scope
+//now we want to understand why it happened the inner working of it
+//so we do debugging and tun the js debugger 
 
 //=====================================================================================
 
@@ -29,3 +41,9 @@ function sayName(){
 
 console.log('Value of fname is', fname)
 sayName() //Yes
+
+//but here output is
+//Value of fname is Piyush
+//In SayName Hitesh
+//why and how?
+//why when a global variable is defined the function picks up the updated value of same variable inside the function and not the global variable 
