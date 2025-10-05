aedc671f2dc41147c6002f0e2a961da22e0e8825
dhrubb <dhrubbgupta@gmail.com>
2025-10-05 14:43:43 +0530

    added the feature of one todo task being identified by a board and then getting appending to it

--- 6th_Week_JS_Project/script.js
+++ 6th_Week_JS_Project/script.js
@@ -9,7 +9,31 @@ addTaskBtn.addEventListener('click', ()=> {
     const taskCard = document.createElement('p')
     taskCard.classList.add('item')
     taskCard.innerText = input
-    
+    taskCard.setAttribute('draggable', true)
+
     todoBoard.appendChild(taskCard)
 
 })
+
+// const allBoards = document.getElementsByClassName('board')
+const allBoards = document.querySelectorAll('.board')
+const allItems = document.querySelectorAll('.item')
+
+allItems.forEach(item => {
+    item.addEventListener('dragstart', ()=> {
+        item.classList.add('flying')
+    })
+    item.addEventListener('dragend', ()=> {
+        item.classList.remove('flying')
+    })
+})
+
+allBoards.forEach(board => {
+    board.addEventListener('dragover', ()=>{
+        const flyingElement = document.querySelector('.flying')
+        console.log(board, 'Kuch toh mere upar se gya', flyingElement)
+
+        board.appendChild(flyingElement)
+    })
+})
+
