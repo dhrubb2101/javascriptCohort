efdccf965a9f2aca603a37fa01a73b10358d18be
dhrubb <dhrubbgupta@gmail.com>
2025-10-12 11:50:57 +0530

    usage of nodemon

--- 7th_Week_FullStackProject/index.js
+++ 7th_Week_FullStackProject/index.js
@@ -4,10 +4,28 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Cohort')
 })
+app.get('/hitesh', (req, res) => {
+  res.send('Hitesh')
+})
+app.get('/piyush', (req, res) => {
+  res.send('Piyush')
+})
+//Common Error here we are going to make 
+//always add slash '/' in the starting of the url
+//http://localhost:3000/hitesh
+//http://localhost:3000/piyush
+
+//you need to restart if you make any changes in the code
+//way to do speed run
+
+//get type ki request / pr aa rhi hai
+//thr callback pura jo syntax hai voh express ke thorugh dia ja rha hai
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+//app is a web server that listens on port 3000 and responds with "Hello World!" to requests to the root URL (/).
+//To run this code, you need to have Node.js and the Express package installed. You can install Express using npm (Node Package Manager) with the command npm install express. Then, you can run the code using node index.js (assuming you save it in a file named index.js).
